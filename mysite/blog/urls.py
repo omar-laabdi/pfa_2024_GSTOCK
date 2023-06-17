1f7d630cbb35d1cfe2a038cec5ddc5eaf67389c7
Salim23457 <salimbensaber@gmail.com>
2023-06-17 17:51:11 +0100

    ;

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,17 +4,17 @@ from . import views
 
 
 urlpatterns = [
-   path('home/', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('', views.login, name='login'),
-    path('provider', views.provider, name='provider'),
+    path('provider/', views.provider, name='provider'),
     path('edit_provider/<provide_id>', views.edit_provider, name='edit_provider'),
     path('new_provider', views.new_provider, name='new_provider'),
     path('delete_provider/<provide_id>', views.delete_provider, name='delete_provider'),
-    path('article', views.article, name='article'),
+    path('article/', views.article, name='article'),
     path('edit_article/<article_id>', views.edit_article, name='edit_article'),
-    path('new_article', views.new_article, name='new_article'),
+    path('new_article/', views.new_article, name='new_article'),
     path('delete_article/<article_id>', views.delete_article, name='delete_article'),
-    path('client', views.client, name='client'),
+    path('client/', views.client, name='client'),
     path('edit_client/<client_id>', views.edit_client, name='edit_client'),
     path('new_client', views.new_client, name='new_client'),
     path('delete_client/<client_id>', views.delete_client, name='delete_client'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from blog.models import Provider, Article ,Client
-
-
+from django.contrib.auth.decorators import login_required
+from blog.models import Provider, Article ,Client
 
 def home(request):
     return redirect('login')
